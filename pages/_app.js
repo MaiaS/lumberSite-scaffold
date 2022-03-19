23e88ae3529892261064d0a94d9071302f805886
Maia Sason (work) <maia@lumber.dev>
2022-03-19 01:21:07 -0400

    favi

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,44 @@
 import "../styles/index.css";
 import { ThemeProvider } from "theme-ui";
 import theme from "../src/utils/theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
   return (
-    <ThemeProvider theme={theme}>
-      {getLayout(<Component {...pageProps} />)}
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Lumber</title>
+        <meta name="description" content="Lumber Site" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#FFCC08"
+        />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
+    </>
   );
 }
 
