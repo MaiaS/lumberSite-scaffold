7795bed54ef1407d390529e1c50fee781712645b
MaiaS <90791391+MaiaS@users.noreply.github.com>
2022-03-21 16:36:15 -0400

    hard coded svg in rings for better performance

--- src/components/MainFeature/ring.jsx
+++ src/components/MainFeature/ring.jsx
@@ -3,8 +3,13 @@ import { useRef, useEffect, useState, memo } from "react";
 import { useMedia } from "react-use";
 import { Box, Flex, Text } from "theme-ui";
 import { motion } from "framer-motion";
+import RingSvg from "./ring1.svg";
+import RingSvg2 from "./ring2.svg";
+import RingSvg3 from "./ring3.svg";
+import RingSvg4 from "./ring4.svg";
+import RingSvg5 from "./ring5.svg";
 
-const RingFeature = ({ list, title }) => {
+const RingFeature = ({ title }) => {
   const ringRef = useRef(null);
   const cursorRef = useRef(null);
 
@@ -80,6 +85,7 @@ const RingFeature = ({ list, title }) => {
         height: "100%",
         width: "100%",
         overflow: "hidden",
+
         ".textpath": {
           fontSize: "3px",
         },
@@ -100,6 +106,7 @@ const RingFeature = ({ list, title }) => {
       }}
     >
       <img
+        sx={{ zIndex: 99 }}
         ref={cursorRef}
         className="mover"
         src="/assets/cursor/HoverCursor.svg"
@@ -115,45 +122,30 @@ const RingFeature = ({ list, title }) => {
         onViewportEnter={() => setStart(true)}
         onViewportLeave={() => setStart(false)}
       >
-        {start && <RingSet list={list} title={title} />}
+        {start && <RingSet title={title} />}
       </motion.div>
     </Box>
   );
 };
 
-const RingSet = memo(function RingSet({ list, title }) {
-  const [ringAnimation, setRingAnimation] = useState(false);
-  const animationDelay = 4.6;
+const RingSet = memo(function RingSet({ title }) {
   return (
     <Flex
       sx={{
-        width: "25%",
+        width: "50%",
         position: "absolute",
         transition: "1s ease",
         justifyContent: "center",
         alignItems: "center",
-        isolation: "isolate",
       }}
     >
-      {list.slice().map((li, i) => {
-        return (
-          <Ringlet
-            key={`${li + i}`}
-            li={li}
-            i={i + 1}
-            list={list}
-            setRingAnimation={i === list.length - 1 && setRingAnimation}
-            ringAnimation={ringAnimation}
-            animationDelay={animationDelay}
-          />
-        );
-      })}
+      <RingCoded />
 
       <Box
         sx={{
           zIndex: 100,
           aspectRatio: "1",
-          width: "100%",
+          width: "50%",
         }}
       >
         <motion.div
@@ -163,7 +155,7 @@ const RingSet = memo(function RingSet({ list, title }) {
             justifyContent: "center",
             alignItems: "center",
             height: "100%",
-            width: "100%",
+            width: "50%",
             zIndex: 3,
           }}
           initial={{ opacity: 0, translateY: 4 }}
@@ -173,7 +165,7 @@ const RingSet = memo(function RingSet({ list, title }) {
           <Text
             sx={{
               textAlign: "center",
-              fontSize: ["8px", "clamp(25px, 2vw,42px)"],
+              fontSize: ["clamp(25px, 2vw,42px)"],
               whiteSpace: "nowrap",
 
               fontWeight: 900,
@@ -182,173 +174,104 @@ const RingSet = memo(function RingSet({ list, title }) {
             {title}
           </Text>
         </motion.div>
-        <motion.svg
-          initial={{ scale: 0.06, originX: "50%", originY: "50%" }}
-          transition={{ delay: 1, type: "spring", duration: 0.5 }}
-          animate={{ scale: 1 }}
-          width="100%"
-          height="100%"
-          className="brand"
-          viewBox="0 0 100 100"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="50" cy="50" r="50" />
-        </motion.svg>
       </Box>
+      <motion.svg
+        initial={{ scale: 0.06, originX: "50%", originY: "50%" }}
+        transition={{ delay: 1, type: "spring", duration: 0.5 }}
+        animate={{ scale: 1 }}
+        sx={{
+          position: "absolute",
+          borderRadius: "50%",
+          pointerEvents: "none",
+          overflow: "hidden",
+          background: "brand",
+          width: ["70%", "50%"],
+          aspectRatio: "1",
+        }}
+      ></motion.svg>
     </Flex>
   );
 });
 
-const Ringlet = ({ li, i, list, animationDelay }) => {
-  const getCircumference = (radius) => {
-    return 2 * Math.PI * radius;
-  };
-
-  const getChar = (circumference, fontSize) => {
-    if (!circumference || !fontSize) return 1;
-    return circumference / fontSize;
-  };
-
-  /** Generate a textPath to set inside a text svg element. Takes string, radius of ring, and fontSize
-   * 1st get circumference
-   * 2nd get max chars that text can occupy based on circumference length
-   * 3rd get amount of times to multiply string
-   * 4th padstart string to give space between repeated strings, repeat by `newChars`
-   * return textpath with new string.
-   */
-  const generateString = (string, radius, fontSize) => {
-    const circumference = getCircumference(radius);
-
-    const maxChars = getChar(circumference, fontSize);
-    const newChars = Math.max(1, maxChars / (string.length + 1));
-    const newString = string.padStart(string.length + 1, " ").repeat(newChars);
-
-    return `<textPath
-
-    method=stretch
-   spacing=exact
-   textLength=155%
-    xlink:href="#circle"
-    >
-    ${newString}
-    </textPath>`;
-  };
-
-  const hoverRef = useRef();
-  // const rotationStart = Math.floor(Math.random() * 360);
-  // const scale = 2.25 + Math.pow(i, 1.2 + i * 0.025);
-
+// hard-coded
+const RingCoded = () => {
+  const [animation, setAnimation] = useState(false);
+  const array = [RingSvg, RingSvg2, RingSvg3, RingSvg4, RingSvg5];
   return (
-    <motion.div
-      // onAnimationComplete={
-      //   setRingAnimation ? () => setRingAnimation(true) : null
-      // }
-      initial={{ scale: 0, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      transition={{ type: "spring", delay: (i + 3) * 0.2, duration: 0.2 }}
+    <Box
       sx={{
-        "@keyframes rotation": {
-          "0%": {
-            transform: "rotate(360deg)",
-          },
-          "100%": {
-            transform: "rotate(0deg)",
-          },
-        },
-
-        aspectRatio: "1",
-        width: "100%",
-        position: "absolute",
-        transition: "1s ease",
-        animation: `rotation ${Math.max(20, 20 * i * 0.5)}s infinite`,
-        animationDelay: `${animationDelay}s`,
-        animationTimingFunction: "linear",
-        zIndex: list.length - i,
-        ":hover": {
-          animationPlayState: "paused",
-        },
-        svg: {
-          circle: {
-            pointerEvents: "auto",
-          },
-        },
+        isolation: "isolate",
       }}
     >
-      {/* <Box
-        sx={{
-          aspectRatio: "1",
-          height: `${120 * i * 0.5 + 10}%`,
-          width: `${120 * i * 0.5 + 10}%`,
-          top: `${50}%`,
-          left: `${50}%`,
-          borderRadius: "50%",
-          transform: "translate(-50%,-50%)",
-          position: "absolute",
-          border: i % 2 === 0 ? "1px solid" : "1px dotted",
-          strokeDashoffset: "20px",
-          borderColor: "brand",
-        }}
-      ></Box> */}
-      <svg
-        key={`${li}-${i}`}
-        style={{
-          pointerEvents: "none",
-          position: "absolute",
-          height: `${130 * (i + 1)}%`,
-          width: `${130 * (i + 1)}%`,
-          top: `${50}%`,
-          left: `${50}%`,
-          transform: "translate(-50%,-50%)",
+      {array.map((e, i) => (
+        <Box
+          key={`${e}-${i}`}
+          sx={{
+            "@keyframes rotation": {
+              "0%": {
+                transform: "rotate(360deg)",
+              },
+              "100%": {
+                transform: "rotate(0deg)",
+              },
+            },
 
-          //  transform: `scale(${scale}) rotate(${rotationStart}deg)`,
-          // transformStyle: "preserve-3d",
-        }}
-        preserveAspectRatio="none"
-        fill="none"
-        viewBox="0 0 100 100"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          id="circle"
-          d="
-          M 25, 50
-          a 1,1 0 1,1 50,0
-          a 1,1 0 1,1 -50,0
-          "
-
-          // d="
-          // M cx cy
-          // m -r, 0
-          // a r,r 0 1,0 (r * 2),0
-          // a r,r 0 1,0 -(r * 2),0
-          // "
-        />
-        <circle
-          onMouseEnter={() => {
-            hoverRef.current.style.fill = "var(--theme-ui-colors-brand)";
+            zIndex: array.length - (i + 1),
+            top: "50%",
+            left: "50%",
+            position: "absolute",
+            pointerEvents: "none",
+            transform: "translate(-50%, -50%)",
           }}
-          onMouseLeave={() => {
-            hoverRef.current.style.fill = "white";
-          }}
-          className="circle"
-          fill="black"
-          strokeWidth={`.1px`}
-          strokeDasharray={(i + 1) % 2 === 0 ? "0" : ".3"}
-          cx="50"
-          cy="50"
-          r={`${25 * Math.pow(1.6, Math.pow(0.5, 0.3 * (i + 1)))}`}
-        />
-
-        <text
-          ref={hoverRef}
-          fill="white"
-          className="textpath"
-          dangerouslySetInnerHTML={{
-            __html: generateString(li, 50, 3),
-          }}
-        />
-      </svg>
-    </motion.div>
+        >
+          <motion.div
+            onAnimationComplete={
+              i === array.length - 1 ? () => setAnimation(true) : null
+            }
+            sx={{
+              width: ["50vw", "30vw"],
+              height: "100%",
+              borderRadius: "50%",
+              overflow: "hidden",
+              zIndex: array.length - (i + 1),
+              border: "1px",
+              borderStyle: i % 2 === 0 ? "solid" : "dotted",
+              borderColor: "brand",
+              py: `${5 / (i + 1)}%`,
+              px: `${5 / (i + 1)}%`,
+              background: "black",
+              pointerEvents: "all",
+              svg: {
+                transition: ".3s ease",
+                fill: "white",
+              },
+              ":hover": {
+                svg: {
+                  fill: "brand",
+                  animationPlayState: "paused",
+                },
+              },
+            }}
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{
+              scale: Math.pow(4, 0.2 * i),
+              opacity: 1,
+            }}
+            transition={{
+              type: "spring",
+              delay: (i + 7) * 0.2,
+              duration: 0.2,
+            }}
+          >
+            <div
+              sx={{ animation: animation && "rotation 10s linear infinite" }}
+            >
+              {e()}
+            </div>
+          </motion.div>
+        </Box>
+      ))}
+    </Box>
   );
 };
 
